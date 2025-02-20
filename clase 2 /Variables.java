eb6bf6dfb1ce50e4e05ae5c2b76407aa039a68f6
johanfernando11 <johan.sanchezrinco@campusucc.edu.co>
2025-02-20 23:14:05 +0000

    clase2

---
+++ clase 2 /Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    //metodo main
+    public static void main(String[] args) {
+        //tipos de datos
+        //enteros int, long
+        int edad = 25;
+        //reales double, float
+        double altura = 1.75;
+        //cadenas
+        String nombre = "Juan";
+         //logicos
+        boolean estudiante = true; //false
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ", Altura: " + altura + ", Estudiante: " + estudiante);
+    }
+}
+
---
+++ clase 2 /condicionales.java
@@ -0,0 +1,13 @@
+public class condicionales {
+    public static void main(String[] args) {
+        int numero = 0;
+
+        if (numero > 0) {
+            System.out.println("El numero es positivo");
+        } else if (numero < 0) {
+            System.out.println("El numero es negativo");
+        } else {
+            System.out.println("El numero es cero");
+        }
+    }
+}
